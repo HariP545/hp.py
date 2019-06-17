b28869c49022f65cdbe96c0b26659c68e87a6a9c
HariP545 <51898891+HariP545@users.noreply.github.com>
2019-06-17 11:57:05 +0530

    Update hppp.py

--- hppp.py
+++ hppp.py
@@ -3,6 +3,6 @@ if((p>='a' and p<='z') or (p>='A' and p<='Z')):
     if(p=='a' or p=='A' or p=='e'or p=='E'or p=='i'or p=='I'or p=='o'or p=='O'or p=='u'or p=='U'):
         print("vowel")
     else:
-        print("consonant")
+        print("Consonant")
 else:
     print("invalid")
